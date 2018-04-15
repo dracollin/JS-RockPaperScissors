398758db0c4a61cb146676051254de867a40664c
Pedro Chies Collin <20824727+dracollin@users.noreply.github.com>
2018-04-14 23:07:53 -0300

    Work in progress 2.
    -Just finished the JS and the CSS.
    -Look pretty and fluent, easy to understand and more easy to play.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,89 @@
+// GLOBAL.
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
+
+//Pick a random choice for the COMPUTER.
+function getComputerChoice() {
+	const choices = ['r', 'p', 's'];
+	const randomNumber = Math.floor(Math.random() * 3);
+	return choices[randomNumber];
+}
+
+//Convert to another word.
+function convertToWord(letter) {
+	if (letter ===  "r") return "Rock";
+	if (letter === "p") return "Paper";
+	return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+	const smallUserWord = "user".fontsize(3).sub();
+	const smallCompWord = "comp".fontsize(3).sub();
+	const userChoice_div = document.getElementById(userChoice);
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`;
+	userChoice_div.classList.add('green-glow');
+	setTimeout(() => userChoice_div.classList.remove('green-glow'), 300);
+}
+
+function lose(userChoice, computerChoice) {
+	const smallUserWord = "user".fontsize(3).sub();
+	const smallCompWord = "comp".fontsize(3).sub();
+	const userChoice_div = document.getElementById(userChoice);
+	computerScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}. You lose!`;
+	userChoice_div.classList.add('red-glow');
+	setTimeout(() => userChoice_div.classList.remove('red-glow'), 300);
+}
+
+function draw(userChoice, computerChoice) {
+	const smallUserWord = "user".fontsize(3).sub();
+	const smallCompWord = "comp".fontsize(3).sub();
+	const userChoice_div = document.getElementById(userChoice);
+	result_p.innerHTML = `${convertToWord(userChoice)}  its equals ${convertToWord(computerChoice)}. It's a draw!`;
+	userChoice_div.classList.add('gray-glow');
+	setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
+}
+
+//Return the USER choice.
+function game(userChoice) {
+	const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) { //When USER choice one, returns WIN, LOSE or DRAW.
+  	case "rs":
+  	case "pr":
+  	case "sp":
+  	win(userChoice, computerChoice);
+  	break;
+  	case "rp":
+  	case "ps":
+  	case "sr":
+  	lose(userChoice, computerChoice);
+  	break;
+  	case "rr":
+  	case "pp":
+  	case "ss":
+  	draw(userChoice, computerChoice);
+  	break;
+  }
+}
+
+//Creates the button choice action for the USER.
+function main() {
+	rock_div.addEventListener('click', () => game("r"));
+	paper_div.addEventListener('click', () => game("p"));
+	scissors_div.addEventListener('click', () => game("s"));
+}
+
+main();
